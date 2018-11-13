3b3c603d62a4c725a55cda7ecbb6f0a341d38a34
JulienBe <bertozzijulien@gmail.com>
2018-11-13 15:04:52 +0100

    Free moving paddle

--- src/systems/user_controlled.rs
+++ src/systems/user_controlled.rs
@@ -21,23 +21,11 @@ impl<'s> System<'s> for UserControlled {
         // Iterate over all planks and move them according to the input the user
         // provided.
         for (paddle, transform) in (&paddles, &mut transforms).join() {
-//            let opt_movement = match paddle.side {
-//                Side::Left => input.axis_value("left_paddle"),
-//                Side::Right => input.axis_value("right_paddle"),
-//            };
-            let opt_movement = input.axis_value("player_control");
-
-            if let Some(movement) = opt_movement {
-                use ARENA_HEIGHT;
+            if let Some(movement) = input.axis_value("vertical") {
                 transform.translate_y(paddle.velocity * time.delta_seconds() * movement as f32);
-
-                // We make sure the paddle remains in the arena.
-                let paddle_y = transform.translation().y;
-                transform.set_y(
-                    paddle_y
-                        .max(paddle.height * 0.5)
-                        .min(ARENA_HEIGHT - paddle.height * 0.5),
-                );
+            }
+            if let Some(movement) = input.axis_value("horizontal") {
+                transform.translate_x(paddle.velocity * time.delta_seconds() * movement as f32);
             }
         }
     }
